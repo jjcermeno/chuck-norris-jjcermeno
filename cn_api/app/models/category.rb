157d76f8cc834751f2cef700dfab67588e4e0f7a
Juan José Cermeño Portet <jjcerpo@gmail.com>
2020-11-05 17:32:11 +0100

    syntax fix

--- cn_api/app/models/category.rb
+++ cn_api/app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   has_many :joke_categories
-  has_many :jokes, :through => :joke_categories
+  has_many :jokes, through: :joke_categories
 
   validates_presence_of :category_name
   validates_uniqueness_of :category_name
